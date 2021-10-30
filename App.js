9b08cfe9a8036fb32a9da28f65c046bbd2014b78
JerryAgbo <90285713+JerryAgbo@users.noreply.github.com>
2021-10-30 18:54:34 +0000

    pages added

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
+import React from 'react';
+import {Text, View}from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Homepage from './Screens/home';
+import Login from './Screens/login';
+import { render } from 'react-dom';
 
-export default function App() {
+const Stack = createNativeStackNavigator()
+
+const  App = () => {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Login'>
+          <Stack.Screen name="Homepage" component={Homepage} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
 }
+     
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#ecf00f1',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
